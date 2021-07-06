56a02ffc4b9886c3df448adc6f2ea970a37d7eab
sustainedtang <77056068+sustainedtang@users.noreply.github.com>
2021-07-05 19:40:23 -0700

    a

--- app.js
+++ app.js
@@ -1,7 +1,11 @@
-//Use D3 fetch to read JSON file
-d3.json("samples.json").then((importedData) => {
-    console.log(importedData);
-    var data = importedData;
+function buildData(sample) {
+    //Use D3 fetch to read JSON file
+    d3.json("samples.json").then((importedData) => {
+        console.log(importedData);
+        var data = importedData.metadata;
+        var array = metadata.filter(sampleobject => sampleobject.id == sample);
+        var result = array[0]
 
     //Sort 
 })
+}
